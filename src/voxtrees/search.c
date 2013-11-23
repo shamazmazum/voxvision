1023a8387e4e5256e457bf688ef38fa1631328a8
Vasily Postnicov <shamaz.mazum@gmail.com>
2013-11-23 21:59:05 +0400

    Do less dot copying (just a matter of style)

--- src/voxtrees/search.c
+++ src/voxtrees/search.c
@@ -49,17 +49,18 @@ static int compare_tagged (vox_dot origin, struct tagged_coord *c1, struct tagge
 // Maybe flollowing deserves a bit more explanation
 vox_uint vox_ray_tree_intersection (struct vox_node *tree, const vox_dot origin, const vox_dot dir, vox_dot res, vox_uint depth, vox_tree_path path)
 {
-    vox_dot inter, tmp;
+    vox_dot tmp;
     vox_uint interp, i;
+    struct tagged_coord plane_inter[VOX_N+1];
 
     assert (depth < VOX_MAX_DEPTH);
     path[depth-1] = tree;
 
     if (!(VOX_FULLP (tree))) return 0;
-    if (!(hit_box (tree->bb_min, tree->bb_max, origin, dir, inter))) return 0;
+    if (!(hit_box (tree->bb_min, tree->bb_max, origin, dir, plane_inter[0].coord))) return 0;
     if (depth == vox_lod) // Desired level of details reached -> intersection found
     {
-        vox_dot_copy (res, inter);
+        vox_dot_copy (res, plane_inter[0].coord);
         return depth;
     }
     
@@ -80,12 +81,8 @@ vox_uint vox_ray_tree_intersection (struct vox_node *tree, const vox_dot origin,
 #else
             sum_vector (leaf.dots[i], vox_voxel, tmp);
 #endif
-            interp = hit_box (leaf.dots[i], tmp, origin, dir, inter);
-            if (interp)
-            {
-                vox_dot_copy (intersect[count], inter);
-                count++;
-            }
+            interp = hit_box (leaf.dots[i], tmp, origin, dir, intersect[count]);
+            if (interp) count++;
         }
         if (count)
         {
@@ -97,12 +94,10 @@ vox_uint vox_ray_tree_intersection (struct vox_node *tree, const vox_dot origin,
     // ELSE
 
     vox_inner_data inner = tree->data.inner;
-    struct tagged_coord plane_inter[VOX_N+1];
     int plane_counter = 1;
     // Init tagged_coord structure with "entry point" in the node, so to say
     // The tag is a subspace index of entry point
-    plane_inter[0].tag = get_subspace_idx (inner.center, inter);
-    vox_dot_copy (plane_inter[0].coord, inter);
+    plane_inter[0].tag = get_subspace_idx (inner.center, plane_inter[0].coord);
 
     // Now, search for intersections of the ray and all N axis-aligned planes
     // for our N-dimentional space.
