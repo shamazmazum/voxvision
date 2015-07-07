b0d7a8483b3d3cacd68afabb5b8777205cfe5d9d
Vasily Postnicov <shamaz.mazum@gmail.com>
2015-07-07 21:01:28 +0300

    Exploit local properties of octrees with GCD

--- src/voxrnd/renderer.c
+++ src/voxrnd/renderer.c
@@ -57,30 +57,42 @@ void vox_render (struct vox_rnd_ctx *ctx)
     int w = surface->w;
     int h = surface->h;
     vox_camera_interface *camera = ctx->camera;
-
+    int n = w*h;
     float *origin = camera->get_position (camera->camera);
 
-    /* const struct vox_node *leaf = NULL; */
-    dispatch_apply (w*h, dispatch_get_global_queue (DISPATCH_QUEUE_PRIORITY_HIGH, 0),
-                    ^(size_t p){
+    /*
+      Render the scene running multiple tasks in parallel.
+      Each task renders a stipe 1x4 pixels (when not crossing window border).
+      Inside each task we try to render the next pixel using previous leaf node,
+      not root scene node, if possible
+    */
+    dispatch_apply (n>>2, dispatch_get_global_queue (DISPATCH_QUEUE_PRIORITY_HIGH, 0),
+                    ^(size_t p1){
+                        const struct vox_node *leaf = NULL;
                         vox_dot dir, inter;
-                        int interp;
-                        int i = p/w;
-                        int j = p - i*w;
-                        camera->screen2world (camera->camera, dir, w, h, j, i);
-#if 0
-                        interp = 0;
-                        if (leaf != NULL)
-                            interp = vox_ray_tree_intersection (leaf,  origin, dir, inter, NULL);
-                        if (interp == 0)
-                            interp = vox_ray_tree_intersection (tree, origin, dir, inter,  &leaf);
-#else
-                        interp = vox_ray_tree_intersection (ctx->scene, origin, dir, inter, NULL);
-#endif
-                        if (interp)
+                        int interp, p2;
+                        p1 <<= 2;
+                        for (p2=0; p2<4; p2++)
                         {
-                            Uint32 color = get_color (surface->format, inter, ctx->mul, ctx->add);
-                            pixels[p] = color;
+                            int p = p1 + p2;
+                            if (p >= n) break;
+                            int i = p/w;
+                            int j = p - i*w;
+                            camera->screen2world (camera->camera, dir, w, h, j, i);
+#if 1
+                            interp = 0;
+                            if ((leaf != NULL) && (leaf != ctx->scene))
+                                interp = vox_ray_tree_intersection (leaf,  origin, dir, inter, NULL);
+                            if (interp == 0)
+                                interp = vox_ray_tree_intersection (ctx->scene, origin, dir, inter, &leaf);
+#else
+                            interp = vox_ray_tree_intersection (ctx->scene, origin, dir, inter, NULL);
+#endif
+                            if (interp)
+                            {
+                                Uint32 color = get_color (surface->format, inter, ctx->mul, ctx->add);
+                                pixels[p] = color;
+                            }
                         }
                     });
     SDL_Flip (surface);
