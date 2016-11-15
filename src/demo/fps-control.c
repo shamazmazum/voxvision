2b4e6f4bd61dbdec8e547bfa9f45d2fdb745a0c5
Vasily Postnicov <shamaz.mazum@gmail.com>
2016-11-15 20:40:00 +0300

    Update FPS controller delay each frame rather than each second

--- src/demo/fps-control.c
+++ src/demo/fps-control.c
@@ -2,9 +2,19 @@
 #include <Block.h>
 #include "fps-control.h"
 
+/*
+ * This FPS controller is not very smart. Will work on it later.
+ * It takes into account the rendering time of the previous frame
+ * to calculate a delay delta for the current frame. The only good
+ * thing in it is that it has negative feedback, i.e. if the frame was
+ * rendered faster than desired, delay delta is positive and the next
+ * frame will be rendered slower, and, on the other side, if it was
+ * rendered slower than desired, the next will be rendered faster.
+ */
 fps_controller_t make_fps_controller (int fps)
 {
-    __block Uint32 time = SDL_GetTicks();
+    __block Uint32 total_time = SDL_GetTicks();
+    __block Uint32 frame_time = total_time;
     __block int counter = 0;
     __block int delay = 0;
     fps_controller_t controller = ^{
@@ -13,27 +23,32 @@ fps_controller_t make_fps_controller (int fps)
          * It will put a rendering loop to sleep when needed
          * and also return fps count each second.
          */
-        counter++;
-        Uint32 diff = SDL_GetTicks() - time;
         int actual_fps = 0;
-        int delta;
+        long delta, mul;
+        Uint32 new_time;
 
-        if (diff > 1000)
+        counter++;
+        SDL_Delay ((Uint32)delay);
+        new_time = SDL_GetTicks();
+
+        if (fps)
         {
-            if (fps)
-            {
-                delta = counter - fps;
-                delta *= (1000 - delay*counter);
-                delta /= counter*counter;
-                delay += delta;
-                if (delay < 0) delay = 0;
-            }
+            frame_time = new_time - frame_time;
+            delta = frame_time - delay;
+            mul = (1<<16) - (fps<<16)*(long)frame_time/1000;
+            delta *= mul;
+            delta >>= 16;
+            delay += (int)delta;
+            if (delay < 0) delay = 0;
+        }
 
+        if (new_time - total_time > 1000)
+        {
             actual_fps = counter;
             counter = 0;
-            time = SDL_GetTicks();
+            total_time = new_time;
         }
-        SDL_Delay ((Uint32)delay);
+        frame_time = SDL_GetTicks();
 
         return actual_fps;
     };
