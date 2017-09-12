f091d4a16a631df51bd297fa02675b346a1bace4
Vasily Postnicov <shamaz.mazum@gmail.com>
2017-09-12 19:49:33 +0300

    Fix voxengine's READ_DOT macro which caused store forwarding stalls with -DSSE_INTRIN

--- src/voxengine/modules.h
+++ src/voxengine/modules.h
@@ -1,14 +1,23 @@
 #ifndef MODULES_H
 #define MODULES_H
 
-#define READ_DOT(dot,idx) do {                  \
-        lua_geti (L, idx, 1);                   \
-        lua_geti (L, idx, 2);                   \
-        lua_geti (L, idx, 3);                   \
-        dot[0] = luaL_checknumber (L, -3);      \
-        dot[1] = luaL_checknumber (L, -2);      \
-        dot[2] = luaL_checknumber (L, -1);      \
-        lua_pop (L, 3);                         \
+#ifdef SSE_INTRIN
+#define store_dot(dot, x, y, z) _mm_store_ps (dot, _mm_set_ps (0, z, y, x))
+#else
+#define store_dot(dot, x, y, z) do {            \
+        dot[0] = x; dot[1] = y; dot[2] = z;     \
+    } while (0)
+#endif
+
+#define READ_DOT(dot,idx) do {                    \
+        lua_geti (L, idx, 1);                     \
+        lua_geti (L, idx, 2);                     \
+        lua_geti (L, idx, 3);                     \
+        float t1 = luaL_checknumber (L, -3);      \
+        float t2 = luaL_checknumber (L, -2);      \
+        float t3 = luaL_checknumber (L, -1);      \
+        store_dot (dot, t1, t2, t3);              \
+        lua_pop (L, 3);                           \
     } while (0)
 
 #define WRITE_DOT(dot) do {                                 \
