dbcf2f2fa84f92d2398e44c126555ad3205028d3
Vasily Postnicov <shamaz.mazum@gmail.com>
2018-12-22 19:37:32 +0300

    Replace lua_seti (lua_geti) with lua_rawseti (lua_rawgeti)

--- src/voxengine/modules.h
+++ src/voxengine/modules.h
@@ -7,9 +7,9 @@
 #endif
 
 #define READ_DOT(dot,idx) do {                    \
-        lua_geti (L, idx, 1);                     \
-        lua_geti (L, idx, 2);                     \
-        lua_geti (L, idx, 3);                     \
+        lua_rawgeti (L, idx, 1);                  \
+        lua_rawgeti (L, idx, 2);                  \
+        lua_rawgeti (L, idx, 3);                  \
         float t1 = luaL_checknumber (L, -3);      \
         float t2 = luaL_checknumber (L, -2);      \
         float t3 = luaL_checknumber (L, -1);      \
@@ -18,9 +18,9 @@
     } while (0)
 
 #define READ_DOT_3(idx, x, y, z) do {             \
-        lua_geti (L, idx, 1);                     \
-        lua_geti (L, idx, 2);                     \
-        lua_geti (L, idx, 3);                     \
+        lua_rawgeti (L, idx, 1);                  \
+        lua_rawgeti (L, idx, 2);                  \
+        lua_rawgeti (L, idx, 3);                  \
         x = luaL_checknumber (L, -3);             \
         y = luaL_checknumber (L, -2);             \
         z = luaL_checknumber (L, -1);             \
@@ -32,9 +32,9 @@
         lua_pushnumber (L, dot[0]);                         \
         lua_pushnumber (L, dot[1]);                         \
         lua_pushnumber (L, dot[2]);                         \
-        lua_seti (L, -4, 3);                                \
-        lua_seti (L, -3, 2);                                \
-        lua_seti (L, -2, 1);                                \
+        lua_rawseti (L, -4, 3);                             \
+        lua_rawseti (L, -3, 2);                             \
+        lua_rawseti (L, -2, 1);                             \
     } while (0)
 
 #define WRITE_DOT_3(x,y,z) do {                             \
@@ -42,9 +42,9 @@
         lua_pushnumber (L, x);                              \
         lua_pushnumber (L, y);                              \
         lua_pushnumber (L, z);                              \
-        lua_seti (L, -4, 3);                                \
-        lua_seti (L, -3, 2);                                \
-        lua_seti (L, -2, 1);                                \
+        lua_rawseti (L, -4, 3);                             \
+        lua_rawseti (L, -3, 2);                             \
+        lua_rawseti (L, -2, 1);                             \
     } while (0)
 
 struct cameradata
