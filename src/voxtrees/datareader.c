ae66e9966f5cfaaac00b916f9b4909819f2af735
Vasily Postnicov <shamaz.mazum@gmail.com>
2019-05-03 08:20:56 +0300

    vox_read_raw_data(): allow error arg be NULL

--- src/voxtrees/datareader.c
+++ src/voxtrees/datareader.c
@@ -12,9 +12,15 @@ struct vox_node* vox_read_raw_data (const char *filename, unsigned int dim[],
                                     unsigned int samplesize, int (^test)(unsigned int sample),
                                     const char **error)
 {
+#define SETERROR(x) if (error != NULL) *error = (x)
+#define SETSYSERROR() if (error != NULL) *error = strerror(errno)
+
     struct vox_node *tree = NULL;
     int fd = open (filename, O_RDONLY);
-    if (fd == -1) {*error = strerror (errno); goto done;}
+    if (fd == -1) {
+        SETSYSERROR();
+        goto done;
+    }
     vox_dot *array;
 
 #define BUFSIZE 4096
@@ -27,33 +33,39 @@ struct vox_node* vox_read_raw_data (const char *filename, unsigned int dim[],
     unsigned int value;
 
     struct stat sb;
-    if (fstat (fd, &sb) == -1) {*error = strerror (errno); goto closefd;}
-    if (n*samplesize != sb.st_size) {*error = "Wrong size of dataset"; goto closefd;}
+    if (fstat (fd, &sb) == -1) {
+        SETSYSERROR();
+        goto closefd;
+    }
+    if (n*samplesize != sb.st_size) {
+        SETERROR ("Wrong size of dataset");
+        goto closefd;
+    }
 
     array = vox_alloc (n*sizeof(vox_dot));
-    if (array == NULL) {*error = strerror (errno); goto freemem;}
-    
-    for (i=0; i<dim[0]; i++)
-    {
-        for (j=0; j<dim[1]; j++)
-        {
-            for (k=0; k<dim[2]; k++)
-            {
+    if (array == NULL) {
+        SETSYSERROR();
+        goto freemem;
+    }
+
+    for (i=0; i<dim[0]; i++) {
+        for (j=0; j<dim[1]; j++) {
+            for (k=0; k<dim[2]; k++) {
                 value = 0;
-                for (l=0; l<samplesize; l++)
-                {
-                    if (pos==end)
-                    {
+                for (l=0; l<samplesize; l++) {
+                    if (pos==end) {
                         ssize_t res = read (fd, buf, BUFSIZE);
-                        if (res <= 0) {*error = strerror (errno); goto freemem;}
+                        if (res <= 0) {
+                            SETSYSERROR();
+                            goto freemem;
+                        }
                         end = res;
                         pos = 0;
                     }
                     value |= buf[pos] << (8*l);
                     pos++;
                 }
-                if (test (value))
-                {
+                if (test (value)) {
                     array[counter][0] = i*vox_voxel[0];
                     array[counter][1] = j*vox_voxel[1];
                     array[counter][2] = k*vox_voxel[2];
